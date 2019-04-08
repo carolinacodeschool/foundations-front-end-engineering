8b65dc13c0a172c000c1e16ee5e8c17c27fdbd23
MC3D <m.chapman.home@gmail.com>
2019-04-08 09:41:01 -0400

    updates assignment 5

---
+++ assignments/js/exercises.js
@@ -0,0 +1,32 @@
+// Exercise 1
+// The below function does not work. The variable twoPlusTwo gets set to
+// undefined. Refactor the function to make it work.
+
+function addNumbers(numberA, numberB) {
+  console.log(numberA + numberB);
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo) // 4
+
+// Exercise 2
+// Write a function called yell that logs out an uppercase version of a string.
+
+
+
+// Write a function called yell10 that uses your yell function to log out an
+// uppercase version of a string 10 times.
+
+
+
+// Exercise 3
+// Write a function called longest that returns the longest of two input strings
+//or arrays.
+
+
+
+// Exercise 4
+// Write a function called isVowel that takes a character (i.e. a string of
+// length 1) and returns true if it is a vowel, uppercase or lowercase. The
+// function should return false if the character is not a vowel.
